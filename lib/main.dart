e187067ab1591d8ae432561acf6e23a89c96e24e
krish7x <krishnakumar99g@gmail.com>
2021-03-17 10:13:15 +0530

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text("Welcome!"),
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome!"),
+            backgroundColor: Colors.purple,
           ),
-          backgroundColor: Colors.blueGrey[900],
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                  ),
+                  child: Text(
+                    'Navigation',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text('Messages'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_circle),
+                  title: Text('Profile'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text('Exit'),
+                ),
+              ],
+            ),
+          ),
         ),
-        backgroundColor: Colors.blue,
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://i.guim.co.uk/img/media/892fa5c71f29ce088247397726f32ca83b8231d0/79_0_3386_2031/master/3386.jpg?width=700&quality=85&auto=format&fit=max&s=d73c05f7beca6832352059611aa2a4af'),
-          ),
-        ))));
+      ),
+    );
