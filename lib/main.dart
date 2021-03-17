94e33f2a8fde5f9b67952d26c89f56d60a24d860
krish7x <krishnakumar99g@gmail.com>
2021-03-17 11:14:12 +0530

    single child container

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Welcome!"),
-            backgroundColor: Colors.purple,
-          ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: const <Widget>[
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Colors.purple,
-                  ),
-                  child: Text(
-                    'Navigation',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.message),
-                  title: Text('Messages'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text('Profile'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.exit_to_app),
-                  title: Text('Exit'),
-                ),
-              ],
-            ),
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            height: 150.0,
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0),
+            padding: EdgeInsets.all(25.0),
+            child: Text("hey"),
+            color: Colors.white,
           ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
